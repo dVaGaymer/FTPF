00670ad888c387c60f3de5a475fe8971e7aba7f1
Alvaro <al7aro@gmail.com>
2020-07-16 02:17:51 +0200

    0x instead of 0x0 | but solution is ugly af

--- debug/main.c
+++ debug/main.c
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 17:06:14 by alopez-g          #+#    #+#             */
-/*   Updated: 2020/07/16 01:51:54 by alopez-g         ###   ########.fr       */
+/*   Updated: 2020/07/16 02:14:46 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -947,6 +947,18 @@ int main(int argc, char **argv)
 
         n1 =    printf(	  "BUENO: _HOLA_%05%_HOLA\n");
 	    n0 =    ft_printf( "VERGA: _HOLA_%05%_HOLA\n");
+    	printf(n0 == n1 ? "\033[0;32mOK\033[0m" : "\033[0;31mKO: V:%d B:%d\033[0m", n0, n1);
+        error = n0 == n1 ? error : error + 1;
+        printf("\n\n");
+
+        n1 =    printf(	  "BUENO: _HOLA_%0.p_HOLA\n", 0);
+	    n0 =    ft_printf( "VERGA: _HOLA_%0.p_HOLA\n", 0);
+    	printf(n0 == n1 ? "\033[0;32mOK\033[0m" : "\033[0;31mKO: V:%d B:%d\033[0m", n0, n1);
+        error = n0 == n1 ? error : error + 1;
+        printf("\n\n");
+
+        n1 =    printf(	  "BUENO: _HOLA_%03.p_HOLA\n", 0);
+	    n0 =    ft_printf( "VERGA: _HOLA_%03.p_HOLA\n", 0);
     	printf(n0 == n1 ? "\033[0;32mOK\033[0m" : "\033[0;31mKO: V:%d B:%d\033[0m", n0, n1);
         error = n0 == n1 ? error : error + 1;
         printf("\n\n");
