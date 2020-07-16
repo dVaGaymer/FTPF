2c329a0991f5d9b6b5111b447d430cee9844c22e
Alvaro <al7aro@gmail.com>
2020-07-16 02:28:04 +0200

    % output fixed

--- srcs/p_flags.c
+++ srcs/p_flags.c
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 03:22:53 by alopez-g          #+#    #+#             */
-/*   Updated: 2020/07/16 02:16:58 by alopez-g         ###   ########.fr       */
+/*   Updated: 2020/07/16 02:27:04 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,6 @@ void    apply_p_flags(const char *pos, t_info *si, t_flags *sf, char *str)
 
     sf->width -= 2;
     len = *str == '-' ? ft_strlen(str + 1) : ft_strlen(str);
-    len = *str == 48 ? 0 : len;
-    if (*str == 48)
-        sf->width++;
     neg = *str == '-' ? 1 : 0;
     len_total = sf->prc > ft_strlen(str) - neg ? sf->prc + neg : ft_strlen(str);
     len_str = len_total == 0 || len_total == -1 ? ft_strlen(str) : len_total;
@@ -45,8 +42,9 @@ void    apply_p_flags(const char *pos, t_info *si, t_flags *sf, char *str)
     write(1, "-", *str == '-' ? 1 : 0);
     write(1, "0x", 2);
     space(len_str - ft_strlen(str), 1, si);
-    if (*str != 48)
-        ft_putstr_fd(*str == '-' ? str + 1 : str, 1);
+    *str = !sf->prc && *str == 48 ? ' ' : *str;
+    *str = !sf->prc && *str == 32 && sf->width == 0 ? 0 : *str;
+    ft_putstr_fd(*str == '-' ? str + 1 : str, 1);
     if (sf->neg)
         space(len_total - len_str, 0, si);
     si->t += ft_strlen(str) + 2;
@@ -59,5 +57,4 @@ void    apply_perc(const char *pos, t_info *si, t_flags *sf)
     write(1, "%%", 1);
     if (sf->neg)
         space(sf->width - 1, 0, si);
-    si->t += sf->width;
 }
