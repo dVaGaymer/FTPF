76719d53f6bfcbd82de7872ca0b6ec7d800b80d5
Alvaro <al7aro@gmail.com>
2020-07-28 18:06:40 +0200

    fixed x = X

--- srcs/x_flags.c
+++ srcs/x_flags.c
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 03:22:53 by alopez-g          #+#    #+#             */
-/*   Updated: 2020/07/28 18:00:14 by alopez-g         ###   ########.fr       */
+/*   Updated: 2020/07/28 18:06:14 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void	x_flags(t_info *si, t_flags *sf, char *str)
 
 	len = *str == '-' ? ft_strlen(str + 1) : ft_strlen(str);
 	neg = *str == '-' ? 1 : 0;
+	sf->width = sf->sh && *str != 48 ? sf->width - 2 : sf->width;
 	sf->width = *str != '-' && sf->sep ? sf->width - 1 : sf->width;
 	lt = sf->prc > (int)ft_strlen(str) - neg ? sf->prc + neg : ft_strlen(str);
 	ls = lt == 0 || lt == -1 ? ft_strlen(str) : lt;
@@ -44,7 +45,7 @@ void	x_flags(t_info *si, t_flags *sf, char *str)
 	space(ls - ft_strlen(str), 1, si);
 	*str = !sf->prc && *str == 48 ? ' ' : *str;
 	*str = !sf->prc && *str == 32 && sf->width == 0 ? 0 : *str;
-	si->t = sf->sh ? si->t + 2 : si->t;
+	si->t = sf->sh && *str != 48 ? si->t + 2 : si->t;
 	ft_putstr_fd(*str == '-' ? str + 1 : str, 1);
 	space(sf->neg ? lt - ls : 0, 0, si);
 	si->t += ft_strlen(str) + (*str != '-' && sf->sep ? 1 : 0);
