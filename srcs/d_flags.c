8ff08625f40f3a473c55cdad4ba1b23ea8005a51
Alvaro <al7aro@gmail.com>
2020-07-28 17:22:30 +0200

    norminette

--- srcs/d_flags.c
+++ srcs/d_flags.c
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 00:20:11 by alopez-g          #+#    #+#             */
-/*   Updated: 2020/07/28 16:46:44 by alopez-g         ###   ########.fr       */
+/*   Updated: 2020/07/28 17:20:16 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,21 @@ void	d_flags(t_info *si, t_flags *sf, char *str)
 
 	len = *str == '-' ? ft_strlen(str + 1) : ft_strlen(str);
 	neg = *str == '-' ? 1 : 0;
-	sf->width = *str != '-'  && sf->sep ? sf->width - 1 : sf->width;
+	sf->width = *str != '-' && sf->sep ? sf->width - 1 : sf->width;
 	lt = sf->prc > (int)ft_strlen(str) - neg ? sf->prc + neg : ft_strlen(str);
 	ls = lt == 0 || lt == -1 ? ft_strlen(str) : lt;
 	lt = sf->width > lt ? sf->width : lt;
 	if (sf->zero && sf->prc == -1)
 		ls = lt;
-	if (!sf->neg)
-		space(lt - ls, 0, si);
+	space(!sf->neg ? lt - ls : 0, 0, si);
 	write(1, "-", *str == '-' ? 1 : 0);
 	write(1, &sf->sep, *str != '-' && sf->sep ? 1 : 0);
 	space(ls - ft_strlen(str), 1, si);
 	*str = !sf->prc && *str == 48 ? ' ' : *str;
 	*str = !sf->prc && *str == 32 && sf->width == 0 ? 0 : *str;
 	ft_putstr_fd(*str == '-' ? str + 1 : str, 1);
-	if (sf->neg)
-		space(lt - ls, 0, si);
-	si->t += ft_strlen(str) + (*str != '-'  && sf->sep ? 1 : 0);
+	space(sf->neg ? lt - ls : 0, 0, si);
+	si->t += ft_strlen(str) + (*str != '-' && sf->sep ? 1 : 0);
 	if (str)
 		free(str);
 }
