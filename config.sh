ccc8ec70c54f9f761da4dbc3b173f14ac7d47012
Alvaro Lopez <dvagaymer@MacBook-Pro-de-Alvaro.local>
2020-08-11 05:00:33 +0200

    Fixed / ending in path

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@ FTPF_DIR=./
 export FTPF_DIR
 
 #Absolute path to Libft here:
-LIBFT_DIR=/Users/dvagaymer/Documents/FTPF/srcs/Libft 
+LIBFT_DIR=/Users/dvagaymer/Documents/FTPF/srcs/Libft/ 
 export LIBFT_DIR
